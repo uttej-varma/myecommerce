8a1bd082f690751866f0fd5bd7000478989e44e6
uttej varma <uttej2233@gmail.com>
2023-10-08 00:34:19 +0530

    added productDetails and cartPages\

--- src/features/product-list/ProductList.jsx
+++ src/features/product-list/ProductList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import {
@@ -11,7 +12,7 @@ import {
 } from "@heroicons/react/20/solid";
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 
-import { increment, incrementAsync, selectCount } from "./productListSlice";
+import { increment, incrementAsync, selectCount } from "./productSlice";
 const products = [
   {
     id: 1,
@@ -371,6 +372,7 @@ export default function ProductList() {
                       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
                         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                           {products.map((product) => (
+                           <Link to='/productDetails'>
                             <div key={product.id} className="group relative">
                               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                                 <img
@@ -400,6 +402,7 @@ export default function ProductList() {
                                 </p>
                               </div>
                             </div>
+                          </Link>
                           ))}
                         </div>
                       </div>
