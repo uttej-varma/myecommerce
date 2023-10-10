9bbdcf1f03177516f29bb104949b5f348a2611c8
uttej varma <uttej2233@gmail.com>
2023-10-10 23:31:58 +0530

    added pagination

--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -10,11 +10,10 @@ export function fetchAllProducts(amount = 1) {
   );
 }
 
-export function fetchProductsByFilter({filter,sort,pagination}) {
+export function fetchProductsByFilter({filter,sort}) {
   //filter ={'category':smartphone}
   //filtertobedone={'category}:['smartphone','laptop']
   //sort={_sort:"price",_order=desc}
-  //pagination={_page:1,_limit=10}
   console.log(filter)
   let queryString='';
   for(let key in filter){
@@ -25,17 +24,14 @@ export function fetchProductsByFilter({filter,sort,pagination}) {
     }
   }
   for(let key in sort){
-      queryString+=`${key}=${sort[key]}&`
+      queryString+=`${key}=${filter[key]}&`
   }
-  for(let key in pagination){
-      queryString+=`${key}=${pagination[key]}&`
-  }
+  console.log(queryString)
   return new Promise(async (resolve) =>{
     //ToDo:We will not hard code the URL
     const response=await fetch(`http://localhost:3004/products?${queryString}`);
     const data=await response.json();
-    const totalItems=await response.headers.get('X-Total-Count')
-    resolve({data:{products:data,totalItems:totalItems}});
+    resolve({data});
     
   }
   );
--- src/features/product-list/productSlice.js
+++ src/features/product-list/productSlice.js
@@ -4,7 +4,6 @@ import { fetchAllProducts,fetchProductsByFilter } from './productAPI';
 const initialState = {
   products: [],
   status: 'idle',
-  totalItems:0
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
@@ -16,8 +15,8 @@ export const fetchAllProductsAsync = createAsyncThunk(
 );
 export const fetchProductsByFilterAsync = createAsyncThunk(
   'product/fetchProductsByFilter',
-  async ({filter,sort,pagination}) => {
-    const response = await fetchProductsByFilter({filter,sort,pagination});
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilter({filter,sort});
     return response.data;
   }
 );
@@ -43,8 +42,7 @@ export const productSlice = createSlice({
       })
       .addCase(fetchProductsByFilterAsync.fulfilled, (state, action) => {
         state.status = 'idle';
-        state.products = action.payload.products;
-        state.totalItems=action.payload.totalItems;
+        state.products = action.payload;
       });
   },
 });
@@ -52,6 +50,5 @@ export const productSlice = createSlice({
 export const { increment, decrement } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
-export const totalItems=(state)=>state.product.totalItems;
 
 export default productSlice.reducer;
